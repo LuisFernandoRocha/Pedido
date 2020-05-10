ddd4d34fd8a6cf1d2dbb5fa8268f736daf4f677c
Luis Fernando Rocha <Fernando23dompa2hotmail.com>
2020-05-10 10:20:56 -0300

    projeto pedido

---
+++ Pedido/Entities/Enuns/OrderStatus.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pedido.Entities.Enuns
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
